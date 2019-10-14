32a76b8fef77c92ba9bbbd1031ce60a0324cc834
Anton <antilik@gmail.com>
2019-10-14 22:12:06 +0300

    garbage was deleted from create-react-app and updated package.json as well

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import axios from 'axios';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
   componentDidMount() {
@@ -14,20 +13,7 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
+        <p>Test</p>
       </div>
     );
   }
