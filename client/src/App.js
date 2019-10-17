156acef3048fbaea523f63cd2a30dfa694ad3aa3
@zhana141281 <zhana141281@gmail.com>
2019-10-17 14:21:57 +0300

    Base structure's added

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import axios from 'axios';
 import './App.css';
 
-class App extends React.Component {
-  componentDidMount() {
-    axios.get("/products").then(products => {
-      console.log(products);
-    })
-  }
+import {MainPage} from './components'
+import axios from 'axios';
 
-  render() {
 
+
+
+
+
+// class App extends React.Component {
+//   componentDidMount() {
+//     axios.get("/products").then(products => {
+//       console.log(products);
+//     })
+//   }
+
+//   render() {
+//
+//     return (
+//       <div className="App">
+//         <p>Test</p>
+//       </div>
+//     );
+//   }
+// }
+function App() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-      </div>
-    );
-  }
-}
+        <MainPage/>
+    )
 
+}
 export default App;
---
+++ client/src/components/index.js
@@ -0,0 +1,1 @@
+export {MainPage} from './Mainpage/MainPage'
