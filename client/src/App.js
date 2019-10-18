68642790a5ae30b92c3cc41e32252dbbd803f2ee
Anastasiia Zhmurko <53611771+zhana141281@users.noreply.github.com>
2019-10-18 11:13:11 +0300

    Material (#4)
    * Delete .env
    
    Its not for public file
    
    * Update .gitignore
    
    add .env in .gitignore
    
    * Base structure's added
    
    * Added material-ui global settings, Router to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import {Header} from './commons'
+import {MainPage, Products, SingleProduct, NotFound} from './components'
+
 import axios from 'axios';
-
-import {MainPage} from './components'
-
-
-
-
-
 
 
 // class App extends React.Component {
@@ -28,8 +27,21 @@ import {MainPage} from './components'
 // }
 function App() {
     return (
-        <MainPage/>
+        <Router>
+            <div className="App">
+
+                <Switch>
+                    <Route path={'/'} exact component={MainPage}/>
+                    <Route path={'/products'} exact component={Products}/>
+                    <Route path={'/products/:id'} component={SingleProduct}/>
+                    <Route component={NotFound}/>
+                </Switch>
+
+            </div>
+        </Router>
     )
+
+
 }
 
 
--- client/src/components/Mainpage/MainPage.js
+++ client/src/components/Mainpage/MainPage.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import {Header} from '../../commons'
+
+import '../../index.css'
+
+
+import {withStyles} from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import {Header} from "../../commons";
+
+//global settings for site
+const GlobalCss = withStyles({
+    // @global is handled by jss-plugin-global.
+    '@global': {
+        body: {
+            fontFamily: "Museo Sans"
+        },
+        // You should target [class*="MuiButton-root"] instead if you nest themes.
+        '.MuiButton-root': {
+            background: 'linear-gradient(180deg, #6686FF 0%, #8F8DE2 100%)',
+            borderRadius: '4px',
+        },
+
+    },
+})(() => null);
+
 
 
 export const MainPage = props => {
     return (
-        <Header/>
+        <React.Fragment>
+            <GlobalCss/>
+            <Container maxWidth="md">
+                <Header/>
+            </Container>
+        </React.Fragment>
     )
 }
