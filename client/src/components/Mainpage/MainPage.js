a6a874424d6e8eea7fb43a1fbcd7b935f24c80d0
Anastasiia Zhmurko <53611771+zhana141281@users.noreply.github.com>
2019-10-21 11:34:20 +0300

    Header1 (#10)
    * Delete .env
    
    Its not for public file
    
    * Update .gitignore
    
    add .env in .gitignore
    
    * Base structure's added
    
    * Added material-ui global settings, Router to App.js
    
    * Base header style
    
    * Header is ready

--- client/src/components/Mainpage/MainPage.js
+++ client/src/components/Mainpage/MainPage.js
@@ -1,24 +1,31 @@
 import React from 'react';
 
+import '../../index.css'
+
+
 import {withStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import {Header} from "../../commons";
 import BrandsBlock from "./BrandsBlock";
 
-import '../../index.css'
 
-//global settings for site
 const GlobalCss = withStyles({
-    // @global is handled by jss-plugin-global.
     '@global': {
         body: {
-            fontFamily: "Museo Sans",
-            color: '#444444'
+            fontFamily: "'Museo Sans 500'",
+            color: "#444444"
         },
-        // You should target [class*="MuiButton-root"] instead if you nest themes.
-        '.MuiButton-root': {
+        '.MuiTypography-body2': {
+            fontFamily: "'Museo Sans 500'"
+        },
+        a: {
+            textDecoration: 'none'
+        },
+       '.MuiButton-root': {
             background: 'linear-gradient(180deg, #6686FF 0%, #8F8DE2 100%)',
             borderRadius: '4px',
+            border: 'none',
+            color: '#FFFFFF'
         },
 
     },
@@ -26,14 +33,13 @@ const GlobalCss = withStyles({
 
 
 
-export function MainPage(props) {
+
+export const MainPage = props => {
     return (
         <React.Fragment>
             <GlobalCss/>
-            <Container maxWidth="md">
-                <Header/>
-            </Container>
-            <BrandsBlock/>
+                <Header count={2} callCenter={'1-855-324-5387'}/>
+                <BrandsBlock/>
 
         </React.Fragment>
     )
