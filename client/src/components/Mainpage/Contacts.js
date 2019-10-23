ae014877e8c4e15a63ae48403c0cc5b13188dd46
Kate Petrova <ms.sve4a@gmail.com>
2019-10-24 00:31:13 +0300

    Contacts component

---
+++ client/src/components/Mainpage/Contacts.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+import Typography from "@material-ui/core/Typography";
+import Grid from '@material-ui/core/Grid';
+import Icon from '@material-ui/core/Icon';
+import {withStyles} from '@material-ui/core/styles';
+
+export function Contacts() {
+  return (
+      <div>Hello</div>
+  )
+}
