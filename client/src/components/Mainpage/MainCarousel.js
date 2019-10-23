2f75eccf62d1ce2751578ba9c284a21c5b8bff51
Antilik <48419890+antilik@users.noreply.github.com>
2019-10-23 21:49:47 +0300

    update of main carousel

--- client/src/components/Mainpage/MainCarousel.js
+++ client/src/components/Mainpage/MainCarousel.js
@@ -4,19 +4,23 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 
 export default function MainCarousel() {
+  const carouselStyle = {
+    width: "1000px",
+    height: "550px"
 
+  }
   return (
-    <Carousel autoPlay infiniteLoop showThumbs={false} >
+    <Carousel autoPlay infiniteLoop showThumbs={false}>
       <div>
-        <img src="img/slides/slide01.jpg" alt="Ride with us" />
+        <img src="img/slides/slide01.jpg" alt="Ride with us" style={carouselStyle} />
         <p className="legend">Legend 1</p>
       </div>
       <div>
-        <img src="img/slides/slide02.jpg" alt="Monster of e-bike" />
+        <img src="img/slides/slide02.jpg" alt="Monster of e-bike" style={carouselStyle} />
         <p className="legend">Legend 1</p>
       </div>
       <div>
-        <img src="img/slides/slide03.jpg" alt="Ride it  of e-bike" />
+        <img src="img/slides/slide03.jpg" alt="Ride it  of e-bike" style={carouselStyle} />
         <p className="legend">Legend 1</p>
       </div>
     </Carousel>
