869c2a4ea60118fa07c5348d8fb49fac8b3b8a1b
Fetisova Tetiana <tbabich3003@gmail.com>
2019-10-25 20:56:30 +0300

    eslint fixes

--- client/src/components/Products/Filter.js
+++ client/src/components/Products/Filter.js
@@ -1,6 +1,6 @@
-import React, {Component, useEffect, useState} from 'react';
+import React, { Component, useEffect, useState } from 'react';
 
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -9,43 +9,38 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Select from '@material-ui/core/Select';
 import Checkbox from '@material-ui/core/Checkbox';
 import Chip from '@material-ui/core/Chip';
-import axios from "axios";
+import axios from 'axios';
 
 
-const useStyles = makeStyles(theme => ({
-
+const useStyles = makeStyles((theme) => ({
 
 
 }));
 const Filter = () => {
-    const classes = useStyles();
-    const [filterList, setFilterList]= useState({});
+  const classes = useStyles();
+  const [filterList, setFilterList] = useState({});
 
-    let filters;
+  let filters;
 
-    useEffect(()=> {
-        async function getFilters(){
-            await axios.get("/filters").then(data => {
-               setFilterList(data)
-
-            })
-                .catch(err=>{console.log(err)})
-
-        }
-        getFilters();
-    },[]);
-    if(filterList.data){
-        filters = filterList.data.map(el=>{
-            return <div>{el.type}</div>
-        })
+  useEffect(() => {
+    async function getFilters() {
+      await axios.get('/filters').then((data) => {
+        setFilterList(data);
+      })
+        .catch((err) => { console.log(err); });
     }
+    getFilters();
+  }, []);
+  if (filterList.data) {
+    filters = filterList.data.map((el) => <div>{el.type}</div>);
+  }
 
-    return (
-        <React.Fragment>
+  return (
+    <React.Fragment>
 
-            {filters}
-        </React.Fragment>
-    );
+      {filters}
+    </React.Fragment>
+  );
 };
 
 export default Filter;
--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -12,7 +12,7 @@ import ProductBreadcrumbs from './ProductBreadcrumbs';
 import { Title } from '../Title/Title';
 import StayInTouch from '../../commons/Footer/StayInTouch';
 import Filter from './Filter';
-import Preloader from "../Preloader/Preloader";
+import Preloader from '../Preloader/Preloader';
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -31,82 +31,81 @@ const useStyles = makeStyles((theme) => ({
 export const Products = () => {
   const classes = useStyles();
 
-    const [list, setList] = useState({
-    });
-    const [loading, setLoading] = useState(true);
-    let products;
+  const [list, setList] = useState({
+  });
+  const [loading, setLoading] = useState(true);
+  let products;
 
 
-    useEffect(()=> {
-        async function getList(){
-            await axios.get("/products").then(data => {
-                setList(data);
-                setLoading(false);
-            })
-                .catch(err=>{console.log(err)})
-
-     }
-     getList()
-    },[]);
-    if(list.data && !loading) {
-        products = list.data.map((el)=>{
-            return <Grid item xs={12} sm={4} md={3} key={el.itemNo}>
-                <ProductCard
-                    className={classes.card}
-                    name={el.name}
-                    itemImg={el.imageUrls[0]}
-                    price={el.currentPrice}
-                    url={`products/${el.itemNo}`}
-                    rating={el.rating}
-                />
-            </Grid>
-        })
-    }else{
-        return(
-        <React.Fragment>
-            <Header callCenter={'1-855-324-5387'}/>
-            <Container maxWidth="md" className={classes.paddingTop}>
-                <ProductBreadcrumbs/>
-                <Title title="All products"/>
-                <Typography
-                    variant="body1"
-                    gutterBottom
-                    align="center"
-                    className={classes.space}
-                >
-                    Our full collection of electric devices
-                </Typography>
-                <Filter/>
-                <main>
-                    <Preloader/>
-                </main>
-            </Container>
-            <StayInTouch/>
-        </React.Fragment>
-        )
+  useEffect(() => {
+    async function getList() {
+      await axios.get('/products').then((data) => {
+        setList(data);
+        setLoading(false);
+      })
+        .catch((err) => { console.log(err); });
     }
+    getList();
+  }, []);
+  if (list.data && !loading) {
+    products = list.data.map((el) => (
+      <Grid item xs={12} sm={4} md={3} key={el.itemNo}>
+        <ProductCard
+          className={classes.card}
+          name={el.name}
+          itemImg={el.imageUrls[0]}
+          price={el.currentPrice}
+          url={`products/${el.itemNo}`}
+          rating={el.rating}
+        />
+      </Grid>
+    ));
+  } else {
     return (
-        <React.Fragment>
-            <Header callCenter={'1-855-324-5387'}/>
-            <Container maxWidth="md" className={classes.paddingTop}>
-                <ProductBreadcrumbs/>
-                <Title title="All products"/>
-                <Typography
-                    variant="body1"
-                    gutterBottom
-                    align="center"
-                    className={classes.space}
-                    >
+      <React.Fragment>
+        <Header callCenter="1-855-324-5387" />
+        <Container maxWidth="md" className={classes.paddingTop}>
+          <ProductBreadcrumbs />
+          <Title title="All products" />
+          <Typography
+            variant="body1"
+            gutterBottom
+            align="center"
+            className={classes.space}
+          >
                     Our full collection of electric devices
-                </Typography>
-                <Filter/>
-                <main>
-                    <Grid container spacing={0}>
-                        {products}
-                    </Grid>
-                </main>
-            </Container>
-            <StayInTouch/>
-        </React.Fragment>
-    )
-}
+          </Typography>
+          <Filter />
+          <main>
+            <Preloader />
+          </main>
+        </Container>
+        <StayInTouch />
+      </React.Fragment>
+    );
+  }
+  return (
+    <React.Fragment>
+      <Header callCenter="1-855-324-5387" />
+      <Container maxWidth="md" className={classes.paddingTop}>
+        <ProductBreadcrumbs />
+        <Title title="All products" />
+        <Typography
+          variant="body1"
+          gutterBottom
+          align="center"
+          className={classes.space}
+        >
+                    Our full collection of electric devices
+        </Typography>
+        <Filter />
+        <main>
+          <Grid container spacing={0}>
+            {products}
+          </Grid>
+        </main>
+      </Container>
+      <StayInTouch />
+    </React.Fragment>
+  );
+};
