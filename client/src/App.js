5d3ab4b1968cd7ffa7e1d53e3aa597b563bc0afe
sve4a <37845002+sve4a@users.noreply.github.com>
2019-10-25 13:55:08 +0300

    Sve4a contacts map (#35)
    * Delete .env
    
    Its not for public file
    
    * Update .gitignore
    
    add .env in .gitignore
    
    * Contacts component
    
    * Contacts component
    
    * Contact section
    
    * Merge conflicts fixes
    
    * pkg.json fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,68 @@
 import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {withStyles} from '@material-ui/core/styles';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 import { Provider } from 'react-redux';
 import {
   MainPage, Products, ProductDetails, NotFound,
 } from './components';
 
+import './App.css';
+
 import store from './store/index';
 
 const GlobalCss = withStyles({
-    // @global is handled by jss-plugin-global.
-    '@global': {
-        body: {
-            fontFamily: "'Museo Sans 500'",
-            color: "#444444"
-        },
-
-        a: {
-            textDecoration: 'none'
-        },
-        '.MuiButton-root': {
-            fontFamily: "'Museo Sans 500'",
-            background: 'linear-gradient(180deg, #6686FF 0%, #8F8DE2 100%)',
-            borderRadius: '4px',
-            border: 'none',
-            color: '#FFFFFF'
-        },
-        '.MuiTypography-root': {
-            fontFamily: "'Museo Sans 500'",
-            color: "#444444"
-        },
-        '.MuiTypography-body2': {
-            fontFamily: "'Museo Sans 500'"
-        },
-        '.MuiButtonGroup-groupedContainedPrimary:not(:last-child)': {
-            borderRight: '1px solid #EAEAEA',
-        },
-        '.MuiContainer-maxWidthMd': {
-            maxWidth: '1060px'
-        },
-        '.MuiCardMedia-root': {
-            backgroundSize: 'contain',
-        }
+  // @global is handled by jss-plugin-global.
+  '@global': {
+    body: {
+      fontFamily: "'Museo Sans 500'",
+      color: '#444444',
     },
+
+    a: {
+      textDecoration: 'none',
+    },
+    '.MuiButton-root': {
+      fontFamily: "'Museo Sans 500'",
+      background: 'linear-gradient(180deg, #6686FF 0%, #8F8DE2 100%)',
+      borderRadius: '4px',
+      border: 'none',
+      color: '#FFFFFF',
+    },
+    '.MuiTypography-root': {
+      fontFamily: "'Museo Sans 500'",
+      color: '#444444',
+    },
+    '.MuiTypography-body2': {
+      fontFamily: "'Museo Sans 500'",
+    },
+    '.MuiButtonGroup-groupedContainedPrimary:not(:last-child)': {
+      borderRight: '1px solid #EAEAEA',
+    },
+    '.MuiContainer-maxWidthMd': {
+      maxWidth: '1060px',
+    },
+    '.MuiCardMedia-root': {
+      backgroundSize: 'contain',
+    },
+  },
 })(() => null);
 
 function App() {
-    return (
-        <Provider store={store}>
-          <Router>
-            <GlobalCss/>
-            <div className="App">
-                <Switch>
-                    <Route path={'/'} exact component={MainPage}/>
-                    <Route path={'/products'} exact component={Products}/>
-                    <Route path={'/products/:id'} component={ProductDetails}/>
-                    <Route component={NotFound}/>
-                </Switch>
-            </div>
-          </Router>
-        </Provider>
-    )
-
-
+  return (
+    <Provider store={store}>
+      <Router>
+        <GlobalCss />
+        <div className="App">
+          <Switch>
+            <Route path="/" exact component={MainPage} />
+            <Route path="/products" exact component={Products} />
+            <Route path="/products/:id" component={ProductDetails} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
+  );
 }
 
 export default App;
