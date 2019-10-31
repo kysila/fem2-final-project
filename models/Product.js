b29b06e8cba90180bfea678299e14cc803e980c9
akysil <tyoma.ksl@gmail.com>
2019-10-31 18:21:15 +0200

    edit priduct model in back end

--- models/Product.js
+++ models/Product.js
@@ -90,7 +90,10 @@ const ProductSchema = new Schema(
       type: Number,
       required: true,
       default: 0
-    }
+    },
+    features: {
+        type: Schema.Types.Mixed,
+    },
   },
   { strict: false }
 );
